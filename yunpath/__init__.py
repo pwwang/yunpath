8e3f29da0235bd2e22b4d677681c6d6ee811d4c3
pwwang <pwwang@pwwang.com>
2025-11-18 21:00:06 -0700

    fix: suppress FutureWarning for unsupported Python version

--- yunpath/__init__.py
+++ yunpath/__init__.py
@@ -1,3 +1,14 @@
+import sys
+
+# FutureWarning: You are using a Python version (3.10.19) which Google will stop
+# supporting in new releases of google.api_core once it reaches its end of life
+# (2026-10-04).
+# Please upgrade to the latest Python version, or at least Python 3.11,
+# to continue receiving updates for google.api_core past that date.
+if sys.version_info < (3, 11):  # pragma: no cover
+    import warnings
+    warnings.filterwarnings("ignore", category=FutureWarning)
+
 from cloudpathlib.anypath import AnyPath
 from cloudpathlib.azure.azblobclient import AzureBlobClient
 from cloudpathlib.azure.azblobpath import AzureBlobPath
